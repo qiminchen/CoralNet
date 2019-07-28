5ea395b9f71408a96cab559dc3547cd59bde860c
qiminchen <liam951120@gmail.com>
2019-07-28 16:25:58 -0700

    Update __init__.py
    generalize to any models

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,16 @@
 import importlib
 
 
-def get_model(alias, test=False):
-    module = importlib.import_module('models.' + alias)
-    if test:
-        return module.Model_test
-    return module.Model
+def get_model(opt):
+    module = importlib.import_module('models.' + opt.net)
+    if opt.net == 'resnet50':
+        model = module.resnet50(opt.pretrained, opt.nclasses)
+        if opt.sets == 'target':
+            assert opt.ntclasses > 0
+            model = module.replace_classifier(model, opt.net_path,
+                                              opt.ntclasses)
+    elif opt.net == 'efficientnet':
+        pass
+    else:
+        raise NotImplementedError()
+    return model
