4d5f10af21308e8efee27bcd9fb12468f89a2b2b
qiminchen <liam951120@gmail.com>
2019-07-27 19:13:44 -0700

    Create util_metric.py
    utility for computing the loss and accuracy

---
+++ util/util_metric.py
@@ -0,0 +1,28 @@
+import torch
+
+
+class MetricUtil:
+    def __init__(self):
+        self.running_loss = 0.0
+        self.running_accuracy = 0
+        self.pred = []
+        self.gt = []
+
+    def compute_metric(self, model, phase, inputs, labels,
+                       criterion, optimizer, nbatch):
+        with torch.set_grad_enabled(phase == 'train'):
+            outputs = model(inputs)
+            _, predicts = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+            # backward and optimize only if in training phase
+            if phase == 'train':
+                loss.backward()
+                optimizer.step()
+            elif phase == 'valid':
+                self.pred += predicts
+                self.gt += labels.data
+        # statistics
+        self.running_loss += loss.item()
+        self.running_accuracy += torch.sum(predicts == labels.data)
+        return (self.running_loss / nbatch,
+                self.running_accuracy / (nbatch * inputs.size(0)))
