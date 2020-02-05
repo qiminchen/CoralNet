e0e53580bfe09b10103aa97713ed901bc4615f88
qimin <liam951120@gmail.com>
2020-02-04 20:19:28 -0800

    Update coralnet_extraction.py

--- datasets/coralnet_extraction.py
+++ datasets/coralnet_extraction.py
@@ -1,5 +1,6 @@
 import os
 import json
+import torch
 from os.path import join
 from PIL import Image
 from PIL import ImageFile
@@ -14,40 +15,45 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class Dataset(data.Dataset):
 
+    data_root = '/home/qimin/Downloads/evaluation/images'
+
     transform = transforms.Compose([
         transforms.ToTensor(),
     ])
-    img_size = 224
 
-    @classmethod
-    def read_bool_status(cls, status_file):
-        with open(status_file) as f:
-            lines = f.read()
-        return [x == 'True' for x in lines.split('\n')]
-
-    def __init__(self, opt, mode='valid', local=False):
-        assert mode in ['train', 'valid']
-        self.mode = mode
+    def __init__(self, opt, local=False):
+        source = opt.source
         self.local = local
 
-        # Read sources list
-        s3 = boto3.resource('s3', endpoint_url="https://s3.nautilus.optiputer.net")
-        bucket = s3.Bucket('qic003')
-        self.bucket = bucket
+        images_list = os.listdir(os.path.join(self.data_root, source))
+
+        # Pack paths into a dict
+        annotations = []
+        for i, im in enumerate(images_list):
+            im_root = os.path.join(self.data_root, source, im)
+            anno_list = os.listdir(im_root)
+            anns = [os.path.join(im_root, al) for al in anno_list]
+            labels = [int(al.split('_')[2]) for al in anno_list]
+            assert len(anns) == len(labels)
+            save_dir = os.path.join(opt.logdir, source, 'images')
+            anno_dict = {'image_dir': im_root,
+                         'anns_path': anns,
+                         'anns_labels': labels,
+                         'anns_save_dir': os.path.join(save_dir, im + '.anns.npy'),
+                         'feat_save_dir': os.path.join(save_dir, im + '.features.npy')}
+            annotations.append(anno_dict)
+        self.annotations = annotations
 
     def __getitem__(self, idx):
-        return None
+        image = self.annotations[idx]
+        loaded_images = []
+        for anno in image['anns_path']:
+            img = Image.open(anno).convert('RGB')
+            img = self.transform(img)
+            loaded_images.append(img)
+        image['anns_loaded'] = torch.stack(loaded_images)
+        image['anns_labels'] = torch.tensor(image['anns_labels'])
+        return image
 
     def __len__(self):
-        return 1
-
-
-def collate_data(batch):
-    # images, labels = zip(*batch)
-    len_batch = len(batch)
-    batch = list(filter(lambda x: x is not None, batch))
-    if len_batch > len(batch):
-        diff = len_batch - len(batch)
-        batch = batch + batch[:diff]
-    # return torch.cat(images), torch.cat(labels)
-    return default_collate(batch)
+        return len(self.annotations)
