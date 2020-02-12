3e2ce960b0cdc6e7b3d49b81a3ec9438739b8a38
qiminchen <liam951120@gmail.com>
2020-02-12 10:14:42 -0800

    simplify .sh files

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,20 +5,20 @@ outdir=../qic003/result
 set -e
 
 python train.py \
-    --net efficientnet \
-    --net_version "b0" \
+    --net resnet \
+    --net_version "resnet50" \
     --pretrained \
     --fine_tune \
     --dataset coralnet_nautilus \
     --nclasses 1279 \
     --gpu -2 \
-    --batch_size 72 \
+    --batch_size 64 \
     --wdecay 3e-6 \
     --log_time \
     --optim sgd \
     --lr 0.05 \
     --max_lr 0.1 \
     --epoch 5 \
-    --workers 32 \
+    --workers 24 \
     --logdir "$outdir" \
     $*
