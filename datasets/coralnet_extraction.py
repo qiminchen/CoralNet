be4ba81293fcbe4ffd5e51ee579d599a721060c2
qimin <liam951120@gmail.com>
2020-02-28 21:17:02 -0800

    features extraction sanity check

--- datasets/coralnet_extraction.py
+++ datasets/coralnet_extraction.py
@@ -27,18 +27,20 @@ class Dataset(data.Dataset):
         for i, im in enumerate(images_list):
             im_root = os.path.join(self.data_root, source, im)
             anno_list = os.listdir(im_root)
-            anns = [os.path.join(im_root, al) for al in anno_list]
-            labels = [int(al.split('_')[2]) for al in anno_list]
-            anno_loc = [{'row': al.split('_')[-2], 'col': al.split('_')[-1]} for al in anno_list]
-            assert len(anns) == len(labels)
-            anno_dict = {'image_dir': im_root,
-                         'anns_path': anns,
-                         'anns_labels': labels,
-                         'anno_loc': anno_loc,
-                         'anns_save_path': im + '.features.anns.npy',
-                         'feat_save_path': im + '.features.json',
-                         'anno_loc_path': im + '.anno.loc.json'}
-            annotations.append(anno_dict)
+            if len(anno_list) > 0:
+                anns = [os.path.join(im_root, al) for al in anno_list]
+                labels = [int(al.split('_')[2]) for al in anno_list]
+                anno_loc = [{'row': al.split('_')[-2],
+                             'col': al.split('_')[-1].split('.')[0]} for al in anno_list]
+                assert len(anns) == len(labels)
+                anno_dict = {'image_dir': im_root,
+                             'anns_path': anns,
+                             'anns_labels': labels,
+                             'anno_loc': anno_loc,
+                             'anns_save_path': im + '.features.anns.npy',
+                             'feat_save_path': im + '.features.json',
+                             'anno_loc_path': im + '.anno.loc.json'}
+                annotations.append(anno_dict)
         self.annotations = annotations
 
     def __getitem__(self, idx):
