7556b1f9d86de8b8ac7fa261f74542e51735a13c
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-03-20 09:54:19 -0700

    added simple timing script

---
+++ simple_speed_test.py
@@ -0,0 +1,26 @@
+import torch
+import time
+import numpy as np
+from models.efficientnet import efficientnet
+
+model = efficientnet(False, 'b0', 1000, False, 1024)
+
+batch_sizes = [1, 10]
+patch_sizes = [168, 224]
+for batch_size in batch_sizes:
+    for patch_size in patch_sizes:
+        run_times = []
+        for i in range(10):
+            patch = torch.rand(batch_size, 3, patch_size, patch_size)
+            t0 = time.perf_counter()
+            res = model.extract_features(patch)
+            # If measuring GPU run-times, add this line:
+            # torch.cuda.synchronize()
+            t1 = time.perf_counter()
+            run_times.append((t1 - t0)/batch_size)
+        print('[Batchsize: {}, patch size: {}] '
+              'Time per patch: {:.4f} +- {:.4f} seconds'.
+            format(batch_size,
+                   patch_size,
+                   np.mean(run_times),
+                   np.std(run_times)))
