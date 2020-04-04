a7bfcaf9cc6a1e3f660769968f2ce79eb4aeeac2
qimin <liam951120@gmail.com>
2020-04-04 16:48:22 -0700

    update get_labels_meta()
    1. add lower_case, upper_case and substring_case of invalid labels
    2. take the duplicates mapping into account when computing ann_count_training and #_sources_present
    3. read stat from .anns.json and label_set.json files instead of from crop export

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,12 +1,13 @@
 import os
 from os.path import join
 import json
+import copy
 import csv
 from collections import Counter
 
 
 data_root = '/media/qimin/seagate5tb/coral'
-stat_root = '/media/qimin/seagate5tb/stat'
+stat_root = '/mnt/sda/coral/beta_status'
 beta_cropped_root = '/media/qimin/samsung1tb/beta_cropped'
 
 invalid_labels = ['Unclear', 'All other', 'Off', 'Fuzz', 'TAPE', 'Unknown', 'Framer', 'off' 'subject',
@@ -65,30 +66,50 @@ def get_labels_meta():
     training_sources = line.split('\n')
 
     # step 1 - merge duplicates
-    duplicates_meta = [i for i in label_meta if i['duplicate_of'] != 'None']
+    duplicates_meta = [i for i in copy.deepcopy(label_meta) if i['duplicate_of'] != 'None']
     for i in duplicates_meta:
         for j in label_meta:
             if j['name'] == i['duplicate_of']:
                 j['ann_count'] += i['ann_count']
-                i['ann_count'] = 0   # soft copy, 'ann_count' in label_meta will be changed as well
+                i['duplicate_of_id'] = j['id']
+    # contains no duplicates
+    label_meta = [i for i in label_meta if i['duplicate_of'] == 'None']
 
-    # step 2 - remove labels with suspicious names
-    label_meta = [i for i in label_meta if i['name'] not in invalid_labels]
+    # step 2 - get labels with suspicious names
+    # consider lower cases and upper cases and substrings
+    lower_cases = [i.lower() for i in invalid_labels]
+    upper_cases = [i.upper() for i in invalid_labels]
+    contain_substr_from_invalid = [i['name'] for i in label_meta
+                                   if any(substr in i['name'] for substr in
+                                          set(invalid_labels).union(lower_cases, upper_cases))]
+    label_removed = [i for i in label_meta if i['name'] in contain_substr_from_invalid]
+    # label_meta = [i for i in label_meta if i['name'] not in contain_substr_from_invalid]
 
     # step 3 - compute ann_count_training and #_sources_present
+    duplicates_dict = {i['id']: i['duplicate_of_id'] for i in duplicates_meta}
     ann_count_training = []
     sources_present = []
-    for source in training_sources:
-        labels = os.listdir(join(beta_cropped_root, source))
-        sources_present += labels
-        for label in labels:
-            ann_count_training += len(os.listdir(join(beta_cropped_root, source, label))) * [label]
+    for idx, source in enumerate(training_sources):
+        anns_list = os.listdir(os.path.join(data_root, source, 'images'))
+        anns_list = [i for i in anns_list if i.endswith('.anns.json')]
+
+        labels = []
+        for i in anns_list:
+            with open(join(data_root, source, 'images', i), 'r') as f:
+                label = json.load(f)
+            labels += [duplicates_dict[i['label']] if i['label'] in duplicates_dict else i['label'] for i in label]
+
+        ann_count_training += labels
+        sources_present += list(set(labels))
+        print('[{}: {} complete]'.format(idx + 1, source))
 
     ann_count_training = dict(Counter(ann_count_training))
     sources_present = dict(Counter(sources_present))
     for i in range(len(label_meta)):
-        label_meta[i]['ann_count_training'] = ann_count_training[str(label_meta[i]['id'])]
-        label_meta[i]['#_sources_present'] = sources_present[str(label_meta[i]['id'])]
+        label_meta[i]['ann_count_in_training'] = ann_count_training[label_meta[i]['id']] if \
+            label_meta[i]['id'] in ann_count_training else 0
+        label_meta[i]['#_sources_present_in_training'] = sources_present[label_meta[i]['id']] if \
+            label_meta[i]['id'] in sources_present else 0
 
     keys = label_meta[0].keys()
     with open(join(stat_root, 'labels_meta.csv'), 'w', newline='') as output_file:
@@ -96,8 +117,14 @@ def get_labels_meta():
         dict_writer.writeheader()
         dict_writer.writerows(label_meta)
 
+    keys = label_removed[0].keys()
+    with open(join(stat_root, 'label_removed.csv'), 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(label_removed)
+
     return True
 
 
 if __name__ == '__main__':
-    ok = get_sources_meta()
+    ok = get_labels_meta()
