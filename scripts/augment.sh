fc6e19cf0a9e405c0aead6258ba98d6c6329290b
qimin <liam951120@gmail.com>
2020-04-09 23:30:05 -0700

    update augment.sh

--- scripts/augment.sh
+++ scripts/augment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source_list=/mnt/sda/coral/beta_status/training_sources.txt
-data_root=/media/qimin/samsung1tb/beta_cropped
+source_list=/mnt/sda/coral/beta_status/test.txt
+data_root=/mnt/sda/beta_cropped
 
-cat ${source_list} | xargs -n1 -P10 -I {} python ./datasets/geometric_augmentation.py -- --data_root ${data_root} {}
+cat ${source_list} | xargs -n1 -P2 -I {} python ./datasets/geometric_augmentation.py -- --data_root ${data_root} {}
