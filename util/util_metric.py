d44a3d06ecc6bcdbf0c88b5015140cbb7192f23c
qimin <liam951120@gmail.com>
2020-04-25 14:48:08 -0700

    augmentation training

--- util/util_metric.py
+++ util/util_metric.py
@@ -9,7 +9,7 @@ class MetricUtil:
         self.gt = []
 
     def compute_metric(self, model, phase, inputs, labels,
-                       criterion, optimizer, nbatch):
+                       criterion, optimizer, lr_scheduler, nbatch):
         with torch.set_grad_enabled(phase == 'train'):
             outputs = model(inputs)
             _, predicts = torch.max(outputs, 1)
@@ -18,6 +18,7 @@ class MetricUtil:
             if phase == 'train':
                 loss.backward()
                 optimizer.step()
+                lr_scheduler.step()
             elif phase == 'valid':
                 self.pred += list(predicts.cpu().numpy())
                 self.gt += list(labels.cpu().numpy())
@@ -25,6 +26,6 @@ class MetricUtil:
         batch_loss = loss.item()
         batch_acc = torch.sum(predicts == labels.data).item() / inputs.size(0)
         self.running_loss += loss.item()
-        self.running_accuracy += torch.sum(predicts == labels.data)
+        self.running_accuracy += torch.sum(predicts == labels.data).item()
         return (batch_loss, batch_acc, self.running_loss / nbatch,
-                self.running_accuracy.item() / (nbatch * inputs.size(0)))
+                self.running_accuracy / (nbatch * inputs.size(0)))
