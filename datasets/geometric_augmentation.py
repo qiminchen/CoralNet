4df7b1a564146c584afa9c4467bbcb890cbe7be5
qimin <liam951120@gmail.com>
2020-05-23 01:26:07 -0700

    geometric pre-augmentation for training LR

--- datasets/geometric_augmentation.py
+++ datasets/geometric_augmentation.py
@@ -14,9 +14,19 @@ parser.add_argument('--data_root', type=str, help='path to beta cropped dir')
 argv = sys.argv[sys.argv.index("--") + 1:]
 args = parser.parse_args(argv)
 
-with open('/mnt/sda/coral/beta_status/labels_rarity.json', 'r') as f:
-    label_rarity = json.load(f)
-labels_id = list(label_rarity.keys())
+
+def _get_k(rarity):
+    # check how many augmented images needed by labels rarity
+    if rarity <= 200:
+        return 7
+    if 200 < rarity <= 400:
+        return 5
+    bounds = [400, 600, 800, 1000]
+    k = 3
+    for idx, (lower, upper) in enumerate(zip(bounds[:-1], bounds[1:])):
+        if lower < rarity <= upper:
+            k = 3 - idx
+    return k
 
 
 def geometric_augmentation(source, data_root):
@@ -25,31 +35,34 @@ def geometric_augmentation(source, data_root):
     :param data_root: path to beta cropped dir
     :return: augmented images
     """
-    labels = os.listdir(join(data_root, source))
-    for label in labels:
-        # if label not in training label, no data augmentation needed
-        # if label has more than 50,000 patches, no data augmentation needed
-        if label not in labels_id or label_rarity[label] > 50000:
+    with open(join('/mnt/sda/features/status', source, 'labels_rarity.json'), 'r') as f:
+        labels_rarity = json.load(f)
+
+    with open(join('/mnt/sda/features/status', source, 'test_list.txt'), 'r') as f:
+        line = f.read()
+    test_list = line.split('\n')
+
+    images = os.listdir(join(data_root, source))
+    for img in images:
+        # test image should not be augmented
+        if img in test_list:
             continue
+        patches = os.listdir(join(data_root, source, img))
+        for patch in patches:
+            label = patch.split('_')[2]
+            if label not in labels_rarity or labels_rarity[label] > 1000:
+                continue
 
-        # check how many augmented images needed by labels rarity
-        bounds = [12500, 14285, 16666, 20000, 25000, 33333, 50000]
-        num_augmented = 7
-        for idx, (lower, upper) in enumerate(zip(bounds[:-1], bounds[1:]), 1):
-            if lower < label_rarity[label] <= upper:
-                num_augmented = 7 - idx
-
-        # augmentation
-        images = os.listdir(join(data_root, source, label))
-        for image in images:
-            img = np.array(Image.open(join(data_root, source, label, image)))
-            # 7 augmented images
-            augmented = [np.rot90(img), np.rot90(img, 2), np.rot90(img, 3), np.fliplr(img),
-                         np.fliplr(np.rot90(img)), np.fliplr(np.rot90(img, 2)), np.fliplr(np.rot90(img, 3))]
-            augmented = random.sample(augmented, k=num_augmented)
+            image = np.array(Image.open(join(data_root, source, img, patch)))
+            k = _get_k(labels_rarity[label])
+            augmented = [np.rot90(image), np.rot90(image, 2), np.rot90(image, 3), np.fliplr(image),
+                         np.fliplr(np.rot90(image)), np.fliplr(np.rot90(image, 2)), np.fliplr(np.rot90(image, 3))]
+            augmented = random.sample(augmented, k=k)
             for i, ai in enumerate(augmented):
-                Image.fromarray(ai).save(join(data_root, source, label,
-                                              image.split('.')[0] + '_da_' + str(i) + '.jpg'))
+                Image.fromarray(ai).save(join(data_root, source, img,
+                                              patch.replace('jpg', 'ga'+str(i)+'.jpg')))
+            del augmented
+
     return True
 
 
